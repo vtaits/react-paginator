f0f558d47d6a023b86d36c73010c9d2ee908eba4
Vadim Taits <taizvadim@gmail.com>
2019-12-16 16:17:24 +0300

    Added example with few pages

--- packages/react-paginator/src/__stories__/index.stories.js
+++ packages/react-paginator/src/__stories__/index.stories.js
@@ -1,8 +1,10 @@
 import { storiesOf } from '@storybook/react';
 
+import FewPages from './FewPages';
 import Simple from './Simple';
 import WithLinks from './WithLinks';
 
 storiesOf('Examples')
   .add('Simple', Simple)
-  .add('WithLinks', WithLinks);
+  .add('With links', WithLinks)
+  .add('Few pages', FewPages);
