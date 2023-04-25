789529f7cdfa5ca1d4675c96107c20f14c36ed08
Vadim Taits <taizvadim@gmail.com>
2023-04-25 14:44:58 +0300

    babel -> tsup

--- packages/react-paginator/jest.config.js
+++ packages/react-paginator/jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   testPathIgnorePatterns: [
     '/node_modules/',
   ],
+
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+
+  transform: {
+    '\\.tsx?$': [
+      'ts-jest',
+    ],
+  },
 };
